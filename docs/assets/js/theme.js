fc836c3f2a8ba1c1e3a8c232cccc9f3741541379
Kitsune Solar <kitsune.solar@gmail.com>
2019-01-27 03:53:00 +0300

    2019-01-27 00:53:00

--- docs/assets/js/theme.js
+++ docs/assets/js/theme.js
@@ -17,7 +17,7 @@ function extJS_getArt() {
 		error: function () {
 		},
 		complete: function () {
-			$('.container').empty().append(out);
+			$('#art').empty().append(out);
 		}
 	}).done(function (data) {
 		message = data;
@@ -37,14 +37,14 @@ function extJS_getArt() {
 				attach_url = attach[k].url;
 
 				if (attach[k].width === undefined) {
-					return 0;
+					out += '';
 				} else {
 					if (count % 2 === 0) out += '<div class="columns">';
 					out += '<div class="column"><figure class="image is-128x128"><img src="' + attach_url + '" alt="' + attach_name + '" /></figure></div>';
 					if (count % 2 !== 0) out += '</div>';
+					count++;
 				}
 
-				count++;
 
 				console.log(count);
 			}

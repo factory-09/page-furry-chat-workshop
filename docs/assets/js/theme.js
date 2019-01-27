5afa5658549e3f306c8a251038ad22184ae5178d
Kitsune Solar <kitsune.solar@gmail.com>
2019-01-27 03:56:00 +0300

    2019-01-27 00:56:00

--- docs/assets/js/theme.js
+++ docs/assets/js/theme.js
@@ -37,19 +37,17 @@ function extJS_getArt() {
 				attach_url = attach[k].url;
 
 				if (attach[k].width === undefined) {
-					return;
+					out += '';
 				} else {
 					if (count % 2 === 0) out += '<div class="columns">';
 					out += '<div class="column"><figure class="image is-128x128"><img src="' + attach_url + '" alt="' + attach_name + '" /></figure></div>';
 					if (count % 2 !== 0) out += '</div>';
+
 					count++;
 				}
-
-
-				console.log(count);
 			}
-
 		}
+
 		if (count % 2 !== 0) out += '</div>';
 	})
 }
